0405e923efa4b67e912d13ae3c6a742af702620d
Hung Le <45841230+hungle2396@users.noreply.github.com>
2023-05-19 23:41:32 -0700

    test

--- routes/google_login.js
+++ routes/google_login.js
@@ -1,6 +1,11 @@
 const passport = require("passport");
 
 module.exports = (app) => {
+    app.get('/', (req, res) => {
+        res.send("Hello World!")
+    });
+
+
     app.get('/auth/google', passport.authenticate('google', {
             scope: ['profile', 'email']
         })
