6df8d43b0b47d90aa845019211b57cf788f7781b
Hung Le <45841230+hungle2396@users.noreply.github.com>
2023-06-11 00:10:54 -0700

    added in google authentication

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import { useFetchUserQuery, useUserLoginMutation } from '../redux/store';
+import GoogleImage from "../img/google.png";
 
 const Login = () => {
   console.log("In Login Component");
@@ -52,43 +53,53 @@ const Login = () => {
   return (
     
     <div className="flex-grow flex justify-center">
-      <div className="form__container flex flex-col w-96 mt-28">
+      <div className="container flex flex-col w-96 mt-28">
         <h2 className="text-center text-4xl font-semibold">Log in</h2>
       {registrationError && <div>{registrationError}</div>}
 
-        <form className="form flex flex-col my-12" onSubmit={handleLogin}>
-          <div className="field-group flex flex-col mb-5">
-            <label className="label" htmlFor="email">Email</label>
-            <input
-              className="input"
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Enter email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          
-          <div className="field-group flex flex-col mb-5">
-            <label className="label" htmlFor="password">Password</label>
-            <input
-              className="input"
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          
-          <button className="btn-primary" disabled={isLoading}>
-            {isLoading ? 'Logging in' : 'Login'}
+        <div className="login__container flex flex-col mt-10 relative">
+          <button className="flex items-center justify-center gap-3 w-full btn-gradient-border text-primary-purple bg-green-300 py-2 px-8 text-center">
+            <a href="/auth/google">Login with Google</a>
+            <img className="w-5" src={GoogleImage} alt="google icon" />
           </button>
-        </form>
+
+          <div className="line-grey"></div>
+
+          <form className="form flex flex-col" onSubmit={handleLogin}>
+            <div className="field-group flex flex-col mb-5">
+              <label className="label" htmlFor="email">Email</label>
+              <input
+                className="input"
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            
+            <div className="field-group flex flex-col mb-5">
+              <label className="label" htmlFor="password">Password</label>
+              <input
+                className="input"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            
+            <button className="btn-primary" disabled={isLoading}>
+              {isLoading ? 'Logging in' : 'Login'}
+            </button>
+          </form>
+        </div>
+        
       
       </div>
     </div>
