fc415f2764e564d0b0fb35ce54f79ddaf4a5a6ce
Hung Le <45841230+hungle2396@users.noreply.github.com>
2023-07-05 18:49:11 -0700

    added in the feature for user to add new event

--- client/src/components/layout/Dashboard.tsx
+++ client/src/components/layout/Dashboard.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-
 import EventForm from '../forms/EventForm';
 import Navigation from '../common/Navigation';
+
 import { ReactComponent as SearchIcon } from '../../UI/img/searchIcon.svg';
+import { ReactComponent as StarsIcon } from '../../UI/img/3-stars.svg';
 import PreviewImage from '../../UI/img/cute_dog.jpg';
+import HappyKid from '../../UI/img/Happy_Kid.jpg';
 
 // import { useFetchUserQuery } from "../../redux/store";
 import { useFetchEventsQuery } from '../../redux/store';
@@ -37,23 +39,27 @@ const Dashboard = () => {
             return (
                 <li 
                     key={event.id}
-                    className='flex'>
-                    <div>
-                        <img src={PreviewImage} className='w-10 h-10 rounded-full' alt='test cute dog'/>
+                    className='flex items-center shadow-box rounded-md px-5 py-5'>
+                    <div className='flex-shrink-0 flex flex-col items-center gap-1'>
+                        <img src={PreviewImage} className='w-14 h-14 rounded-full' alt='test cute dog'/>
     
                         <p className='text-md font-medium'>Ucbi</p>
                     </div>
     
-                    <div>
+                    <div className='mx-5 px-2'>
                         <h1 className='text-xl font-semibold'>{event.name}</h1>
 
                         <p>{event.start_date} - {event.end_date}</p>
+
+                        <p className='mt-2'>{event.description}</p>
                     </div>
     
-                    <div>
-                        <img src={PreviewImage} className='w-14 h-14 rounded-full' alt='preview' />
+                    <div className='flex flex-col items-center gap-1 flex-shrink-0 ml-auto border-transparent border-l-secondary-grey-light border pl-5'>
+                        <StarsIcon className='w-5 h-5 star-icon' />
 
-                        <h1>Sophie</h1>
+                        <img src={HappyKid} className='w-14 h-14 rounded-full' alt='preview' />
+
+                        <h1>Kid</h1>
                     </div>
                 </li>
             )
@@ -87,7 +93,7 @@ const Dashboard = () => {
                 </div>
                 
 
-                <ul className='flex flex-col'>
+                <ul className='flex flex-col my-5 gap-5'>
                     {renderedEvents}
                 </ul>
             </div>
