0a4b1afbee3a422392d8cdf75dfa705f3f60ab71
Hung Le <45841230+hungle2396@users.noreply.github.com>
2023-09-02 23:49:11 -0700

    make the landing page in the center

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="footer bg-transparent mt-auto">
+        <footer className="footer bg-transparent">
             <p className="py-2 text-sm text-gray-600 border-top">Copyright &#169; Goodwill Credit. All rights reserved</p>
         </footer>
     )
